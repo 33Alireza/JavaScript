1a091b826bda0ff4daa5ad26475f88bf14188dbb
Alireza Yousefzadeh <thegeisha2006@gmail.com>
2026-08-10 21:41:57 +0330

    Fundamentals Part One -> Data Types

---
+++ 1-Fundamentals-Part-1/4-DataTypes.js
@@ -0,0 +1,36 @@
+// single line comment
+
+/*
+    multi line comment
+*/
+
+// Primitive Data Types
+let numberDataType = 7;
+let stringDataType = "JavaScript";
+let booleanDataTypeTrue = true;
+let booleanDataTypeFalse = false;
+let undefinedDataType;
+let nullDataType = null;
+let bigIntegerDataType = BigInt(1);
+
+console.log(numberDataType + " " + stringDataType + " " + booleanDataTypeTrue + " " + booleanDataTypeFalse + " " + undefinedDataType + " " + nullDataType + " " + bigIntegerDataType);
+
+let isJavaScriptFun = true
+console.log(isJavaScriptFun);
+
+console.log(typeof true)
+console.log(typeof isJavaScriptFun)
+console.log(typeof nullDataType);
+console.log(typeof stringDataType);
+console.log(typeof "JavaScript");
+
+isJavaScriptFun = "Yes!"
+console.log(typeof isJavaScriptFun)
+
+console.log(undefinedDataType)
+console.log(typeof undefinedDataType)
+
+undefinedDataType = 2006
+console.log(typeof undefinedDataType)
+
+console.log(typeof null)
