bb05b12b85d93821b52251d49f0686e1ce48debd
Alireza Yousefzadeh <thegeisha2006@gmail.com>
2026-08-11 22:38:42 +0330

    Fundamentals Part One -> let const and var

---
+++ 1-Fundamentals-Part-1/5-LetConstAndVar.js
@@ -0,0 +1,14 @@
+let age = 19;
+age = 18;
+
+const birthYear = 2006;
+// birthYear = 2007 -> not allowed
+// const job; -> not Allowed
+
+
+// old way of declaring a variable
+var job = "Developer";
+job = "Coder";
+
+name = "JavaScript";
+console.log(name)
