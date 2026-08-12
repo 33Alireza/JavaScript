4d3ca3d4def5f932bc2df0c701f06729c1dead42
Alireza Yousefzadeh <thegeisha2006@gmail.com>
2026-08-12 16:07:25 +0330

    Fundamentals Part One -> Challenge #3

--- 1-Fundamentals-Part-1/8-Challenges.js
+++ 1-Fundamentals-Part-1/8-Challenges.js
@@ -13,3 +13,17 @@ console.log(markBMI, johnBMI);
 
 if (markHigherBMI) console.log(`Mark's BMI (${markBMI}) is higher than John's (${johnBMI})!`)
 else console.log(`John's BMI (${johnBMI}) is higher than Mark's (${markBMI})!`)
+
+const scoreDolphins = (97 + 112 + 80) / 3;
+const scoreKoalas = (109 + 95 + 50) / 3;
+console.log(scoreDolphins, scoreKoalas);
+
+if (scoreDolphins > scoreKoalas && scoreDolphins >= 100) {
+    console.log('Dolphins win the trophy 🏆');
+} else if (scoreKoalas > scoreDolphins && scoreKoalas >= 100) {
+    console.log('Koalas win the trophy 🏆');
+} else if (scoreDolphins === scoreKoalas && scoreDolphins >= 100 && scoreKoalas >= 100) {
+    console.log('Both win the trophy!');
+} else {
+    console.log('No one wins the trophy 😭');
+}
